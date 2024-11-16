690f33c07460c931660bf071f09681c707f62db7
DavidKent-AppPrgm <56130558+DavidKent-AppPrgm@users.noreply.github.com>
2024-11-16 06:14:43 -0600

    Add files via upload

---
+++ mouseglow.js
@@ -0,0 +1,6 @@
+const glow = document.getElementById('glow');
+
+document.addEventListener('mousemove', (e) => {
+    glow.style.left = `${e.clientX}px`;
+    glow.style.top = `${e.clientY}px`;
+});
