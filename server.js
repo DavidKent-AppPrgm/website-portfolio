6a7b14ca550b6dc49f8c4bcc0fdea0c9abbf1b49
DavidKent-AppPrgm <56130558+DavidKent-AppPrgm@users.noreply.github.com>
2024-11-24 03:11:24 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-//sendMail.js
+//server.js
 const nodemailer = require('nodemailer');
 const { google } = require('googleapis');
 
-const oAuth2Client = new google.auth.oAuth2(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, process.env.GOOGLE_REDIRECT_URI);
+const oAuth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.GOOGLE_REDIRECT_URI
+);
 oAuth2Client.setCredentials({ refresh_token: GOOGLE_REFRESH_TOKEN });
 
 async function sendEmail(event) {
